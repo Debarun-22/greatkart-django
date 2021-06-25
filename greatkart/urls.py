aa658ccfb24e307832b67cf90284b096adb102ab
Debarun Roy <debarunroy2000@gmail.com>
2021-06-25 13:59:18 +0530

    signup page is added

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -24,6 +24,8 @@ urlpatterns = [
     path('',views.home,name='home'),
     path('store/', include('store.urls')),
     path('cart/', include('carts.urls')),
+    path('accounts/', include('accounts.urls')),
+
 
 
 
