1c47a0da510f268466bc4513163ade2a9aec5e9a
Debarun Roy <85675111+Debarun-22@users.noreply.github.com>
2021-07-01 17:05:32 +0530

    Update settings.py

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -151,6 +151,6 @@ MESSAGE_TAGS = {
 #smtp configuration
 EMAIL_HOST ='smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'debarunroy2012@gmail.com'
+EMAIL_HOST_PASSWORD = '8257989724'
 EMAIL_USE_TLS = True
